2d22f2dc7614b23bc85c189ca9ebc35de1ef080e
Jeet-Trueline <jb.truelineinstitute@gmail.com>
2024-09-10 11:35:39 +0530

    10/09/2024

---
+++ calcu.js
@@ -0,0 +1,12 @@
+function add(num1, num2) {
+    console.log("This Is Add Function : " + (num1 + num2));
+}
+
+function sub(num1, num2) {
+    console.log("This Is Sub Function : " + (num1 - num2));
+}
+
+
+module.exports = {
+    add, sub
+}
---
+++ main.js
@@ -0,0 +1,14 @@
+// const sayHello = require("./hello")
+
+
+// sayHello("Trueline inSTITUTE");
+// sayHello``
+
+
+const calcu = require("./calcu");
+
+const num1 = 2;
+const num2 = 5;
+
+calcu.add(num1,num2);
+calcu.sub(num1,num2);
