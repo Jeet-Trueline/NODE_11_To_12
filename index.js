d7af4d0e20485d86f151f9d19823b9a282424898
Jeet-Trueline <jb.truelineinstitute@gmail.com>
2024-09-19 11:22:10 +0530

    19/09/2024

--- index.js
+++ index.js
@@ -28,20 +28,66 @@
 // }).listen(8000);
 
 const express = require("express");
-const { add, sub } = require("./calcu")
+const { MongoClient } = require('mongodb');
+// const { add, sub } = require("./calcu")
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send("Hello...Utsyaa....")
-});
+const url = 'mongodb://localhost:27017';
 
-app.get('/add', (req, res) => {
-    res.send(add(req.query.num1, req.query.num2))
-});
+const client = new MongoClient(url);
 
-app.get('/sub/:num1/:num2', (req, res) => {
-    res.send(sub(req.params.num1, req.params.num2))
-});
+async function main() {
+    // Use connect method to connect to the server
+    await client.connect();
+    // console.log('Connected successfully to server');
+    const db = client.db("demo");
+    const collection = db.collection("tests");
+
+    const data = await collection.find({}).toArray();
+
+    // console.log(data);
+
+    return data;
+}
+
+async function insertData() {
+    await client.connect();
+    // console.log('Connected successfully to server');
+    const db = client.db("demo");
+    const collection = db.collection("tests");
+    const data = await collection.insertOne({ "name": "Harshil1", "age": 10, "email": "harshilvelenja123@gmail.com", "password": "Harshi@123" })
+
+    return data
+}
+
+
+app.post("/insertData", async (req, res) => {
+
+    const insertDatas = await insertData();
+
+    res.send(insertDatas);
+})
+
+app.get("/getData", async (req, res) => {
+
+    const data = await main();
+    console.log(data);
+
+    res.send(data)
+})
+
+
+// app.get('/', (req, res) => {
+//     res.send("Hello......")
+// });
+
+// app.get('/add', (req, res) => {
+//     res.send(add(req.query.num1, req.query.num2))
+// });
+
+// app.get('/sub/:num1/:num2', (req, res) => {
+//     res.send(sub(req.params.num1, req.params.num2))
+// });
 
 app.listen(8000);
 
